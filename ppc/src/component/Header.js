f8197cdbce496a03695f707e06d7c8b3bd601d23
2gacem7 <gacemcode@gmail.com>
2023-03-25 13:57:19 +0100

    Final test

--- ppc/src/component/Header.js
+++ ppc/src/component/Header.js
@@ -1,20 +1,28 @@
 import "./Header.css"
-
 export function Header (props)  {
-  
+    const rules = "La pierre bat les ciseaux en les cassant. Les ciseaux battent la feuille en la coupant. La feuille bat la pierre en l’enveloppant. La partie s’arrête quand un des deux joueurs atteint 3 points."
     return (
         <div className="header">
+            {!props.playing && (
+                <>
+                    <div className="rules"> règles </div>
+                    <div className="rulesCard"> {rules} </div>
+                </>
+            )}
             <div className="container">
-                <div className="headerText">
-                    {props.name}
-                </div>
+                {props.playing && (
+                    <div className="headerText">
+                    {props.name + " " + props.score.player}
+                    </div>
+                )}
                 <div className="headerTitle"> 
                    Shifumi
                 </div>
-                <div className="headerText">
-                     {"Score " + props.score.player + " / " + props.score.ai }
-                </div>
-            
+                {props.playing && (
+                     <div className="headerText">
+                     {"Machine " + " " + props.score.ai }
+                    </div>
+                )}
             </div>
         </div>
     )
--- ppc/src/component/Home.js
+++ ppc/src/component/Home.js
@@ -1,36 +1,32 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
 import {Card } from './Card';
-import { Input } from './Input';
 import "./Home.css"
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 
 export function Home ()  {
+    const name = "Humain";
     const [cardSelect, setCardSelect] = useState(null);
     const [playing, setPlaying] = useState(false);
     const [aiCardSelect, setAiCardSelect] = useState(null);
-    const [score, setScore] = useState(0);
-    const [name, setName] = useState("Chaoshyn");
-    let [round, setRound] = useState(1);
-    const [audioActivate, setAudioActivate] = useState(false)
+    const [continueState, setContinueState] = useState(false);
     let [playerScore, setPlayerScore] = useState(0);
     let [aiScore, setAiScore] = useState(0);
 
-
-   
     var audio = document.createElement('audio');
-    audio.setAttribute('src','balrog.mp3');
- 
+    audio.setAttribute('src','choose2.mp3');
+    
+    var sound = document.createElement('audio');
+    sound.setAttribute('src','fight.mp3');
+
     function startPlaying() {
         if(cardSelect !== null && aiCardSelect !== null) {
+            sound.play();
             setPlaying(true)
-          /*   audio.play(); */
+            setContinueState(true)
             getScore()
         } else {
             alert("Choisis ton champion")
-        }
-        if(playing) {
-            setRound(round += 1)
         }
     }
 
@@ -41,12 +37,10 @@ export function Home ()  {
     function getfight(index) {
         setCardSelect(index)
         setAiCardSelect(getRandomInt(3))
+        audio.play()
     }
 
-
     function getScore () {
-        console.log("player" + cardSelect)
-        console.log("ai" + aiCardSelect);
         if(cardSelect !== null && aiCardSelect !== null) {
             if(cardSelect === aiCardSelect) {
                 return;
@@ -70,132 +64,106 @@ export function Home ()  {
                     setPlayerScore(playerScore += 1)
                 } 
             }
-        }
-       
+        }  
     }
 
-    function compareCard () {
-
-    }
     function ContinuePlaying() {
         setPlaying(false);
-        setShowRoundButton(false)
-        setRound(round += 1);
     }
 
     function stopPlaying() {
         setPlaying(false)
-        setRound(1)
         setAiScore(0)
         setPlayerScore(0)
+        setCardSelect(null)
+        setAiCardSelect(null)
+        setContinueState(false);
     }
       
-    function activateSong() {
-        audio.play();
-        setAudioActivate(true);
-    }
-
-    function disableSong() {
-        audio.preload();
-        setAudioActivate(false);
-    }
-    console.log(cardSelect)
     const gameCard = [{
         src:"scissors.png",
         alt:"scissors",
-        id: 0
     },
     {
         src:"rock.png",
         alt:"rock",
-        id: 1,
     },
     {
         src:"paper.png",
         alt:"paper",
-        id: 2
     }]
-
-    const [showRoundButton,setShowRoundButton] = useState(false)
-    useEffect(()=>{
-        if(!showRoundButton) {
-            setTimeout(function() {
-                setShowRoundButton(true)
-                   }, 4000)}
-        },
-     [])
 
     return (
         <div>
-            <Header name={name} playing={playing} score={{player: playerScore, ai: aiScore}}/>
-            {/* {!audioActivate ? <div onClick={activateSong}> Activer le son </div> : <div onClick={disableSong}> Desactiver le son </div>} */}
+            <Header name={name} playing={continueState} score={{player: playerScore, ai: aiScore}}/>
             <div className="ring">
-                {!name && ( <div className="namingContainer">
-                    <Input />
-                </div>) }
-              
                 {playing ? (
                     <>
-                      {aiScore === 3 || playerScore === 3 ? 
-                      <> 
-                        <div className='winner' style={{display: 'flex', justifyContent:'center', position: 'absolute', color: "white", paddingTop: 40}}>
-                            WINNER
-                            </div>
-                            <div style={{display: 'flex', justifyContent:'center', position: 'absolute', paddingTop: 120}}> 
-                                <img src="ggwp.gif" alt="ggwp" height={150} width={105} />
-                            </div>
+                        {aiScore === 3 || playerScore === 3 ? 
+                            <> 
+                                <div className='winner' style={{display: 'flex', justifyContent:'center', position: 'absolute', color: "white", paddingTop: 40}}>
+                                    WINNER
+                                </div>
+                                <div style={{display: 'flex', justifyContent:'center', position: 'absolute', paddingTop: 120}}> 
+                                    <img src="ggwp.gif" alt="ggwp" height={150} width={105} />
+                                </div>
                             </>
-                             : <></> 
+                             : 
+                            <></> 
                         }
-                <div className='playing'> 
-                        <Card img={gameCard[cardSelect].src} alt={gameCard[cardSelect].alt}/>
-                            <div className="vstext" style={{fontSize: playerScore === 3 ? 60 : 120 }}>  {aiScore === 3 && "AI(E)"}
-                            {playerScore === 3 && name} {aiScore < 3 && playerScore < 3 && "VS"}   </div>
+                        <div className='playing'> 
+                            <Card img={gameCard[cardSelect].src} alt={gameCard[cardSelect].alt}/>
+                            <div className="vstext" style={{fontSize: playerScore === 3 || aiScore === 3 ? 60 : 120 }}>  
+                                {aiScore === 3 && "MACHINE"}
+                                {playerScore === 3 && name} 
+                                {aiScore < 3 && playerScore < 3 && "VS"} 
+                            </div>
                             <Card img={gameCard[aiCardSelect].src} alt={gameCard[aiCardSelect].alt}/>
-                </div>
-                <div className="buttonFight">
-                   {aiScore === 3 || playerScore === 3 ? 
-                     <button className="button" onClick={stopPlaying}>  
-                     <div className="headerText">
-                         FINISH
-                     </div>
-                    </button> : 
-                    <div>
-                        <button className="button" onClick={ContinuePlaying}>  
-                        <div className="headerText">
-                           Continue
                         </div>
-                    </button>
-                 
-                    </div>}
-                </div>
-                </>
+                        <div className="buttonFight">
+                            {aiScore === 3 || playerScore === 3 ? 
+                                <button className="button" onClick={stopPlaying}>  
+                                    <div className="headerText">
+                                        FINISH
+                                    </div>
+                                </button> 
+                            : 
+                                <div>
+                                    <button className="button" onClick={ContinuePlaying}>  
+                                        <div className="headerText">
+                                            Continue
+                                        </div>
+                                    </button>
+                                </div>
+                            }
+                        </div>
+                    </>
                 ) 
-                : (
-                <>
-                <div className="cardcontainer">
-                {gameCard.map((e, index) => {
-                    return (
-                        <div key={index}>
-                        <button className="cardAction" onClick={() => getfight(index)}>
-                        <div className="deckCard" style={{opacity: cardSelect === index ? 1 : 0.7}}>
-                            <Card img={e.src} alt={e.alt}/>
+                : 
+                (
+                    <>
+                        <div className="cardcontainer">
+                            {gameCard.map((e, index) => {
+                                return (
+                                    <div key={index}>
+                                        <button className="cardAction" onClick={() => getfight(index)}>
+                                            <div className="deckCard" style={{opacity: cardSelect === index && 1, marginBottom: cardSelect === index && 60}}>
+                                                <Card img={e.src} alt={e.alt}/>
+                                            </div>
+                                        </button>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    </button>
-                    </div>
-                    )
-                })}
-                </div>
-                <div className="buttonFight">
-                    <button className="button" onClick={startPlaying}>  
-                        <div className="headerText">
-                            FIGHT
+                        <div className="buttonFight">
+                            <button className="button" onClick={startPlaying}>  
+                                <div className="headerText">
+                                    FIGHT
+                                </div>
+                            </button>
                         </div>
-                    </button>
-               </div>
-               </>
-               )}
-               
+                    </>
+               )} 
             </div>
             <Footer />
         </div>
